c0b9d433f20594e49cc649f33445ef40ebcff96c
Nishit Majithia <nishit.nm@gmail.com>
2020-03-03 15:15:21 +0530

    Update download_url in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'SQLtime',
   packages = ['SQLtime'],
-  version = '1.0',
+  version = 'v1.0',
   license='MIT',
   description = 'Python lib to automate time based SQLi',   # Give a short description about your library
   author = 'Nishit',
   author_email = 'nishit@mailfence.com',
   url = 'https://github.com/nishitm/SQLtime',
-  download_url = '',
+  download_url = 'https://github.com/nishitm/SQLtime/archive/v1.0.tar.gz',
   keywords = ['Blind time based SQL injection', 'Pentesting', "AppSec"],
   install_requires=[
           'requests',
